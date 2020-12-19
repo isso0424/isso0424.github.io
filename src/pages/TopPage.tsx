d0bfe1f2251bed6e29e4ac9087a69616759ca3aa
isso0424 <isso04240129@gmail.com>
2020-12-19 20:54:22 +0900

    feat: add style to icon

--- src/pages/TopPage.tsx
+++ src/pages/TopPage.tsx
@@ -3,6 +3,8 @@ import { RouteProps } from "react-router";
 import { Contact } from "../components/Contact";
 import facebook from "../static/facebook-icon.png";
 import twitter from "../static/twitter-logo.svg";
+import icon from "../static/PC_icon.jpg";
+import "./TopPage.scss";
 
 const links = [
   {
@@ -19,9 +21,8 @@ const links = [
 
 export const TopPage: React.FC<RouteProps> = () => (
   <>
-    <header>
-      <h1>isso0424 portfolio</h1>
-    </header>
+    <img src={icon} alt="isso0424 icon" className="top-page-icon" />
+    <h1>isso0424(Kota Isozaki)</h1>
     <p>My portfolio under construction...</p>
     <h3>Contacts</h3>
     <div className="contacts">
