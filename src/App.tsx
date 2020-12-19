f7eb06d716e7e18abc4d18dd06f682f52bd9de68
isso0424 <isso04240129@gmail.com>
2020-12-19 13:56:34 +0900

    fix: why...

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.scss";
 
 const App: React.FC = () => (
   <div className="App">
     <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
       <p>
         Edit
         <code>src/App.tsx</code>
