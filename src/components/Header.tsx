d0d03d1ea1864883d9f0dc3722aaa33e3e66a966
isso0424 <isso04240129@gmail.com>
2020-12-20 00:06:39 +0900

    feat: attach link to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,40 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Header.scss";
 
-const headerContents = ["Top", "About", "Skill", "Works", "Contact"];
+/* "Top", "About", "Skill", "Works", "Contact" */
+const headerContents = [
+  {
+    message: "Top",
+    link: "/",
+  },
+  {
+    message: "About",
+    link: "/",
+  },
+  {
+    message: "Skill",
+    link: "/",
+  },
+  {
+    message: "Works",
+    link: "/",
+  },
+  {
+    message: "Contact",
+    link: "/",
+  },
+];
 
 export const Header: React.FC = () => (
   <div className="header">
     <ul className="header-menu">
       {headerContents.map((content) => (
-        <li className="header-item">
-          <span className="header-text">{content}</span>
-        </li>
+        <Link to={content.link} className="link">
+          <li className="header-item">
+            <span className="header-text">{content.message}</span>
+          </li>
+        </Link>
       ))}
     </ul>
   </div>
