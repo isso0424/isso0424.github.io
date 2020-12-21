c05d622387d2a5859bc937ee3bbece001c35e65d
isso0424 <isso04240129@gmail.com>
2020-12-21 14:43:18 +0900

    chore: stash for battery low

--- src/components/Skill.tsx
+++ src/components/Skill.tsx
@@ -1,17 +1,18 @@
 import React from "react";
 import { Skill as SkillProps } from "../type/skill";
+import "./Skill.scss";
 
 export const Skill: React.FC<SkillProps> = (props: SkillProps) => {
   const { name, imageURL } = props;
 
   return (
     <div className="skill">
-      {name}
       {imageURL != null ? (
         <img alt={name} src={imageURL} className="skill-image" />
       ) : (
         <></>
       )}
+      {name}
     </div>
   );
 };
--- src/pages/SkillsPage.tsx
+++ src/pages/SkillsPage.tsx
@@ -1,3 +1,36 @@
 import React from "react";
+import { SkillsFrame } from "../components/SkillsFrame";
+import { SkillCategory } from "../type/skill";
+import ReactIcon from "../static/react-icon.png";
 
-export const SkillsPage: React.FC = () => <></>;
+const skills: Array<SkillCategory> = [
+  {
+    category: "FrontEnd",
+    skills: [
+      {
+        name: "React",
+        imageURL: ReactIcon,
+      },
+    ],
+  },
+  {
+    category: "BackEnd",
+    skills: [
+      {
+        name: "Node.js",
+      },
+      {
+        name: "Django",
+      },
+    ],
+  },
+];
+
+export const SkillsPage: React.FC = () => (
+  <>
+    <h1>Skills</h1>
+    {skills.map((category) => (
+      <SkillsFrame category={category.category} skills={category.skills} />
+    ))}
+  </>
+);
