e718ad47c8c6714141aa21a785493bc46d0b929a
isso0424 <isso04240129@gmail.com>
2020-12-23 01:26:47 +0900

    feat: attach style to works

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Work.scss";
 
 interface Props {
   title: string;
--- src/pages/WorksPage.tsx
+++ src/pages/WorksPage.tsx
@@ -1,17 +1,41 @@
 import React from "react";
 import { Work } from "../components/Work";
+import "./WorksPage.scss";
 
 const works = [
   {
-    title: "title",
-    text: "text",
+    title: "Rocat",
+    text: "texttexttexttexttexttexttext",
+  },
+  {
+    title: "AdvancedBlackboard",
+    text: "texttexttexttexttexttexttext texttexttexttexttexttexttext",
+  },
+  {
+    title: "IoTMagicCane",
+    text: "texttextexttext",
+  },
+  {
+    title: "ElectricityProtectGame",
+    text: "texttexttext texttexttexttext texttexttexttext texttexttext",
+  },
+  {
+    title: "ElderlyHome",
+    text: "texttexttexttexttexttexttext texttexttexttexttexttexttext",
+  },
+  {
+    title: "Pucis",
+    text: "texttexttexttexttexttexttext texttexttexttexttexttexttext",
   },
 ];
 
 export const WorksPage: React.FC = () => (
   <div className="page">
-    {works.map((work) => (
-      <Work title={work.title} text={work.text} />
-    ))}
+    <h1>Works</h1>
+    <div className="works">
+      {works.map((work) => (
+        <Work title={work.title} text={work.text} />
+      ))}
+    </div>
   </div>
 );
