360707b1b567010dbee66010d4f3139caac25f3d
isso0424 <isso04240129@gmail.com>
2020-12-22 10:51:25 +0900

    feat: attach style to skill components

--- src/components/SkillsFrame.tsx
+++ src/components/SkillsFrame.tsx
@@ -1,16 +1,19 @@
 import React from "react";
 import { SkillCategory } from "../type/skill";
 import { Skill } from "./Skill";
+import "./SkillsFrame.scss";
 
 export const SkillsFrame: React.FC<SkillCategory> = (props: SkillCategory) => {
   const { category, skills } = props;
 
   return (
-    <div className="skill-frame">
+    <>
       <h3>{category}</h3>
-      {skills.map((skill) => (
-        <Skill name={skill.name} imageURL={skill.imageURL} />
-      ))}
-    </div>
+      <div className="skill-frame">
+        {skills.map((skill) => (
+          <Skill name={skill.name} imageURL={skill.imageURL} />
+        ))}
+      </div>
+    </>
   );
 };
