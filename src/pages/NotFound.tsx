2a5e434275b0a8d1918dd49ca97202069a31e10c
isso0424 <isso04240129@gmail.com>
2020-12-24 05:51:49 +0900

    feat: create notfound page

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const NotFoundPage: React.FC = () => (
+  <div>
+    <h1>NotFound</h1>
+    <p>The page is not found.</p>
+    <p>Please check your inputted url.</p>
+  </div>
+);
--- src/pages/TopPage.tsx
+++ src/pages/TopPage.tsx
@@ -3,25 +3,9 @@ import { RouteProps } from "react-router";
 import icon from "../static/PC_icon.jpg";
 import "./TopPage.scss";
 
-/*
-const links = [
-  {
-    url: "https://facebook.com/isso0424",
-    image: facebook,
-    alt: "facebook",
-  },
-  {
-    url: "https://twitter.com/kousou4129",
-    image: twitter,
-    alt: "twitter",
-  },
-];
- */
-
 export const TopPage: React.FC<RouteProps> = () => (
   <div className="page">
     <img src={icon} alt="isso0424 icon" className="top-page-icon" />
     <h1>isso0424(Kota Isozaki)</h1>
-    <p>My portfolio under construction...</p>
   </div>
 );
