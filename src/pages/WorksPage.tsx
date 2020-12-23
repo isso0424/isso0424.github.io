c03956a362e7e4c07a3500fb9f6d3d3dd8c31790
isso0424 <isso04240129@gmail.com>
2020-12-23 11:39:32 +0900

    feat: create works descriptions

--- src/pages/WorksPage.tsx
+++ src/pages/WorksPage.tsx
@@ -5,27 +5,33 @@ import "./WorksPage.scss";
 const works = [
   {
     title: "Rocat",
-    text: "texttexttexttexttexttexttext",
+    text:
+      "It is STEM educational material for junior high school students. It constitutes from 3 components, visual programming tool, graph drawing tool and water rocket. Adopted in MitouJunior",
   },
   {
     title: "AdvancedBlackboard",
-    text: "texttexttexttexttexttexttext texttexttexttexttexttexttext",
+    text:
+      "It is new black board WEB application with AR. You can register documents(images) and draw it with scanning AR-Marker. Developed in OpenHackU2020 vol.1.",
   },
   {
-    title: "IoTMagicCane",
-    text: "texttextexttext",
-  },
-  {
-    title: "ElectricityProtectGame",
-    text: "texttexttext texttexttexttext texttexttexttext texttexttext",
+    title: "ElectricityCollection",
+    text:
+      "It is 2D action game to aim for the goal. You can get dirty (ex: Fire-power) and cleanly(ex: Wind-power) electricity. And, you have to build route with electricity and keep balance dirty electricity and cleanly electricity. Developed in Thailand-Japan Game Programming Hackathon 2020 Final round.",
   },
   {
     title: "ElderlyHome",
-    text: "texttexttexttexttexttexttext texttexttexttexttexttexttext",
+    text:
+      "It is smart speeker to watch over senior citizens. They can manipulate with their voice. If they don't talk with this speeker, it send 'need to check alive' notice for administrator. Developed in Web X IoT Hackathon in Ibaraki.",
   },
   {
     title: "Pucis",
-    text: "texttexttexttexttexttexttext texttexttexttexttexttexttext",
+    text:
+      "It is smart phone application for recording number of missed hours. It can calculate required hours and notify warning of missed hours",
+  },
+  {
+    title: "IoTMagicCane",
+    text:
+      "It is IoT device for controll switch bot. You can controll switch bot with motion.",
   },
 ];
 
