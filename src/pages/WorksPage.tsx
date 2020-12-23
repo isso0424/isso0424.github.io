28a83e0a06577f05b2d1d568a74189588a84f729
isso0424 <isso04240129@gmail.com>
2020-12-23 14:51:45 +0900

    feat: add animation for works page

--- src/pages/WorksPage.tsx
+++ src/pages/WorksPage.tsx
@@ -1,47 +1,86 @@
 import React from "react";
+import { Tab, TabList, TabPanel, Tabs } from "react-tabs";
 import { Work } from "../components/Work";
 import "./WorksPage.scss";
 
-const works = [
+const contestsWorks = [
   {
     title: "Rocat",
     text:
-      "It is STEM educational material for junior high school students. It constitutes from 3 components, visual programming tool, graph drawing tool and water rocket. Adopted in MitouJunior",
+      "STEM educational material for junior high school students. It constitutes from 3 components, visual programming tool, graph drawing tool and water rocket. Adopted in MitouJunior",
   },
   {
-    title: "AdvancedBlackboard",
+    title: "Advanced Blackboard",
     text:
-      "It is new black board WEB application with AR. You can register documents(images) and draw it with scanning AR-Marker. Developed in OpenHackU2020 vol.1.",
+      "New black board WEB application with AR. You can register documents(images) and draw it with scanning AR-Marker. Developed in OpenHackU2020 vol.1.",
   },
   {
-    title: "ElectricityCollection",
+    title: "Electricity Collection",
     text:
-      "It is 2D action game to aim for the goal. You can get dirty (ex: Fire-power) and cleanly(ex: Wind-power) electricity. And, you have to build route with electricity and keep balance dirty electricity and cleanly electricity. Developed in Thailand-Japan Game Programming Hackathon 2020 Final round.",
+      "2D action game to aim for the goal. You can get dirty (ex: Fire-power) and cleanly(ex: Wind-power) electricity. And, you have to build route with electricity and keep balance dirty electricity and cleanly electricity. Developed in Thailand-Japan Game Programming Hackathon 2020 Final round.",
   },
   {
-    title: "ElderlyHome",
+    title: "Elderly Home",
     text:
-      "It is smart speeker to watch over senior citizens. They can manipulate with their voice. If they don't talk with this speeker, it send 'need to check alive' notice for administrator. Developed in Web X IoT Hackathon in Ibaraki.",
-  },
-  {
-    title: "Pucis",
-    text:
-      "It is smart phone application for recording number of missed hours. It can calculate required hours and notify warning of missed hours",
-  },
-  {
-    title: "IoTMagicCane",
-    text:
-      "It is IoT device for controll switch bot. You can controll switch bot with motion.",
+      "Smart speeker to watch over senior citizens. They can manipulate with their voice. If they don't talk with this speeker, it send 'need to check alive' notice for administrator. Developed in Web X IoT Hackathon in Ibaraki.",
   },
 ];
 
-export const WorksPage: React.FC = () => (
-  <div className="page">
-    <h1>Works</h1>
-    <div className="works">
-      {works.map((work) => (
-        <Work title={work.title} text={work.text} />
-      ))}
+const personalWorks = [
+  {
+    title: "Pucis",
+    text:
+      "Smart phone application for recording number of missed hours. It can calculate required hours and notify warning of missed hours",
+  },
+  {
+    title: "IoT Magic Cane",
+    text:
+      "IoT device for controll switch bot. You can controll switch bot with motion.",
+  },
+  {
+    title: "Spoedit",
+    text:
+      "Playlist editor for spotify. You can search musics, add it to playlist and delete musics from playlist.",
+  },
+  {
+    title: "CLI Spotify",
+    text:
+      "Spotify client for CLI in Linux. This client can record playlists and controll player.",
+  },
+];
+
+export const WorksPage: React.FC = () => {
+  const [state, setState] = React.useState({ currentTab: 0 });
+  return (
+    <div className="page">
+      <h1>Works</h1>
+      <Tabs
+        onSelect={(index) => setState({ currentTab: index })}
+        selectedIndex={state.currentTab}
+      >
+        <TabList className="tabList">
+          <Tab className={state.currentTab === 0 ? "selectedTab" : "tab"}>
+            Contests
+          </Tab>
+          <Tab className={state.currentTab === 1 ? "selectedTab" : "tab"}>
+            Personal
+          </Tab>
+        </TabList>
+        <TabPanel>
+          <div className="works">
+            {contestsWorks.map((work) => (
+              <Work title={work.title} text={work.text} />
+            ))}
+          </div>
+        </TabPanel>
+        <TabPanel>
+          <div className="works">
+            {personalWorks.map((work) => (
+              <Work title={work.title} text={work.text} />
+            ))}
+          </div>
+        </TabPanel>
+      </Tabs>
     </div>
-  </div>
-);
+  );
+};
