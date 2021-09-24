005cc46beaeedcc62c748c4f549ada6ce289bd9f
isso0424 <isso04240129@gmail.com>
2021-09-24 13:07:53 +0900

    fix: contact page improvement

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -7,14 +7,21 @@ interface Props {
   image: any;
   alt: string;
   text: string;
+  width: number;
+  height: number;
 }
 
 export const Contact: React.FC<Props> = (props: Props) => {
-  const { url, image, alt, text } = props;
+  const { url, image, alt, text, width, height } = props;
 
   return (
     <a href={url} className="contact-button">
-      <img src={image} alt={alt} className="contact-link" />
+      <img
+        src={image}
+        alt={alt}
+        className="contact-link"
+        style={{ aspectRatio: (width / height).toString() }}
+      />
       <span className="contact-text">{text}</span>
     </a>
   );
--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import { Contact } from "../components/Contact";
 
 import TwitterLogo from "../static/Twitter_Social_Icon_Circle_Color.svg";
-import FacebookLogo from "../static/f_logo_RGB-Blue_1024.png";
-import OctoCat from "../static/Octocat.png";
+import FacebookLogo from "../static/f_logo_RGB-Blue_1024.webp";
+import OctoCat from "../static/Octocat.webp";
 import "./ContactPage.scss";
 
 const contacts = [
@@ -12,18 +12,24 @@ const contacts = [
     alt: "twitter",
     image: TwitterLogo,
     text: "@kousou4129",
+    width: 150,
+    height: 150,
   },
   {
     url: "https://facebook.com/isso0424",
     alt: "facebook",
     image: FacebookLogo,
     text: "KotaIsozaki",
+    width: 200,
+    height: 200,
   },
   {
     url: "https://github.com/isso0424",
     alt: "GitHub",
     image: OctoCat,
     text: "isso0424",
+    width: 241,
+    height: 200,
   },
 ];
 
