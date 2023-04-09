0e8c90d9976e33741d96d196304207b6b765c500
isso0424 <isso04240129@gmail.com>
2023-04-10 06:13:03 +0900

    chore: yarn replace to pnpm

--- src/pages/WorksPage.tsx
+++ src/pages/WorksPage.tsx
@@ -7,46 +7,38 @@ import "./WorksPage.scss";
 const contestsWorks = [
   {
     title: "Rocat",
-    text:
-      "STEM educational material for junior high school students. It constitutes from 3 components, visual programming tool, graph drawing tool and water rocket. Adopted in MitouJunior",
+    text: "STEM educational material for junior high school students. It constitutes from 3 components, visual programming tool, graph drawing tool and water rocket. Adopted in MitouJunior",
   },
   {
     title: "Advanced Blackboard",
-    text:
-      "New black board WEB application with AR. You can register documents(images) and draw it with scanning AR-Marker. Developed in OpenHackU2020 vol.1.",
+    text: "New black board WEB application with AR. You can register documents(images) and draw it with scanning AR-Marker. Developed in OpenHackU2020 vol.1.",
   },
   {
     title: "Electricity Collection",
-    text:
-      "2D action game to aim for the goal. You can get dirty (ex: Fire-power) and cleanly(ex: Wind-power) electricity. And, you have to build route with electricity and keep balance dirty electricity and cleanly electricity. Developed in Thailand-Japan Game Programming Hackathon 2020 Final round.",
+    text: "2D action game to aim for the goal. You can get dirty (ex: Fire-power) and cleanly(ex: Wind-power) electricity. And, you have to build route with electricity and keep balance dirty electricity and cleanly electricity. Developed in Thailand-Japan Game Programming Hackathon 2020 Final round.",
   },
   {
     title: "Elderly Home",
-    text:
-      "Smart speeker to watch over senior citizens. They can manipulate with their voice. If they don't talk with this speeker, it send 'need to check alive' notice for administrator. Developed in Web X IoT Hackathon in Ibaraki.",
+    text: "Smart speeker to watch over senior citizens. They can manipulate with their voice. If they don't talk with this speeker, it send 'need to check alive' notice for administrator. Developed in Web X IoT Hackathon in Ibaraki.",
   },
 ];
 
 const personalWorks = [
   {
     title: "Pucis",
-    text:
-      "Smart phone application for recording number of missed hours. It can calculate required hours and notify warning of missed hours",
+    text: "Smart phone application for recording number of missed hours. It can calculate required hours and notify warning of missed hours",
   },
   {
     title: "IoT Magic Cane",
-    text:
-      "IoT device for controll switch bot. You can controll switch bot with motion.",
+    text: "IoT device for controll switch bot. You can controll switch bot with motion.",
   },
   {
     title: "Spoedit",
-    text:
-      "Playlist editor for spotify. You can search musics, add it to playlist and delete musics from playlist.",
+    text: "Playlist editor for spotify. You can search musics, add it to playlist and delete musics from playlist.",
   },
   {
     title: "CLI Spotify",
-    text:
-      "Spotify client for CLI in Linux. This client can record playlists and controll player.",
+    text: "Spotify client for CLI in Linux. This client can record playlists and controll player.",
   },
 ];
 
