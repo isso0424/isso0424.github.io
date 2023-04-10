b2415423b58bb6ab8f20f275abfcd7bac7f6de1d
isso0424 <isso04240129@gmail.com>
2023-04-10 10:57:52 +0900

    chore: add stylelint to CI

--- src/pages/RightsNotationPage.tsx
+++ src/pages/RightsNotationPage.tsx
@@ -12,8 +12,7 @@ const rights = [
   },
   {
     target: "SASS logo",
-    text:
-      "© 2006–2020 the Sass team, and numerous contributors.\nhttps://creativecommons.org/licenses/by-nc-sa/3.0/deed",
+    text: "© 2006–2020 the Sass team, and numerous contributors.\nhttps://creativecommons.org/licenses/by-nc-sa/3.0/deed",
   },
   {
     target: "HTML logo",
@@ -21,8 +20,7 @@ const rights = [
   },
   {
     target: "AndroidRobot",
-    text:
-      "Android ロボットは、Google が作成および提供している作品から複製または変更したものであり、クリエイティブ・コモンズ表示 3.0 ライセンスに記載された条件に従って使用しています。  https://creativecommons.org/licenses/by/3.0/",
+    text: "Android ロボットは、Google が作成および提供している作品から複製または変更したものであり、クリエイティブ・コモンズ表示 3.0 ライセンスに記載された条件に従って使用しています。  https://creativecommons.org/licenses/by/3.0/",
   },
   {
     target: "Golang logo",
@@ -30,13 +28,11 @@ const rights = [
   },
   {
     target: "TypeScript logo",
-    text:
-      "Copyright (c) 2015 Remo H. Jansen \nReleased under the MIT license\nhttps://github.com/remojansen/logo.ts/blob/master/LICENSE",
+    text: "Copyright (c) 2015 Remo H. Jansen \nReleased under the MIT license\nhttps://github.com/remojansen/logo.ts/blob/master/LICENSE",
   },
   {
     target: "Python logo",
-    text:
-      "https://www.python.org/psf/trademarks/\n https://creativecommons.org/publicdomain/zero/1.0/",
+    text: "https://www.python.org/psf/trademarks/\n https://creativecommons.org/publicdomain/zero/1.0/",
   },
 ];
 
